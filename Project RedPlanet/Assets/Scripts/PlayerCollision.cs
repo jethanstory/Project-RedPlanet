2fae86e09e72750d37f7bf685a8c45aa2a018c31
jethanstory <32407746+jethanstory@users.noreply.github.com>
2022-10-23 11:39:06 -0700

    Project Creation

---
+++ Project RedPlanet/Assets/Scripts/PlayerCollision.cs
@@ -0,0 +1,30 @@
+//Player collision with enemies
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerCollision : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void OnCollisionEnter (UnityEngine.Collision collisionInfo) 
+    {
+        //if (collisionInfo.collider.name == "Enemy" || collisionInfo.collider.name == "WanderingEnemy" || collisionInfo.collider.name == "flyingEnemy" || collisionInfo.collider.name == "longOne" )
+        if (collisionInfo.collider.tag == "Enemy")
+        {
+            Cursor.lockState = CursorLockMode.None;
+            SceneManager.LoadScene(2);
+            Debug.Log("HIT");
+            
+        }
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
+
+/*|| collisionInfo.collider.name == "longOne" || collisionInfo.collider.name == "watcherEnemy" */
